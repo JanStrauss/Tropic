db07c6cd6cb88140cae99f585acf088942328471
s1mpl3x <simplex21@googlemail.com>
2012-03-13 20:25:01 +0100

    added license

--- src/me/simplex/tropic/populators/Populator_Tree_World.java
+++ src/me/simplex/tropic/populators/Populator_Tree_World.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2012 s1mpl3x
+ * 
+ * This file is part of Tropic.
+ * 
+ * Tropic is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Tropic is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Tropic If not, see <http://www.gnu.org/licenses/>.
+ */
 package me.simplex.tropic.populators;
 
 import java.util.HashMap;
